ac2750f98b8d9b12944116b2a1e5c173062e48f2
lucas <wannacry.bat@proton.me>
2025-03-22 11:35:21 -0300

    update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,7 +6,6 @@ int main() {
     int pop1, pop2, area1, area2, pontos_tur1, pontos_tur2;
     float area_km1, area_km2, pib1, pib2;
 
-
     // ---------------dados da primeira carta---------------------
     printf("hora de escrever os dados da primeira carta\n");
     //estado
@@ -18,19 +17,15 @@ int main() {
     //nome
     printf("Digite o nome da cidade da primeira carta: ");
     scanf(" %[^\n]", nome_cidade1);
-
     //pop
     printf("Digite a populacao da primeira carta: ");
     scanf("%d", &pop1);
-
     //area
     printf("Digite a area da primeira carta: ");
     scanf("%f", &area_km1);
-
     //pib
     printf("Digite o pib da primeira carta: ");
     scanf("%f", &pib1);
-
     //num pontos tur
     printf("Digite o numero de pontos turisticos da primeira carta: ");
     scanf("%d", &pontos_tur1);
@@ -42,27 +37,21 @@ int main() {
     //estado
     printf("Digite o nome do estado da segunda carta: ");
     scanf("%s", estado2);
-    
     //cod carta
     printf("Digite o codigo da segunda carta: ");
     scanf("%s", codigo2);
-    
     //nome
     printf("Digite o nome da cidade da segunda carta: ");
     scanf(" %[^\n]", nome_cidade2);
-
     //pop
     printf("Digite a populacao da segunda carta: ");
     scanf("%d", &pop2);
-
     //area
     printf("Digite a area da segunda carta: ");
     scanf("%f", &area_km2);
-
     //pib
     printf("Digite o pib da segunda carta: ");
     scanf("%f", &pib2);
-
     //num pontos tur
     printf("Digite o numero de pontos turisticos da segunda carta: ");
     scanf("%d", &pontos_tur2);
